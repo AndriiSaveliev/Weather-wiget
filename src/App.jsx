016ce860fcf8f14135264ccc34888e0cfc403b4e
Andrii <phsaweliew@gmail.com>
2025-05-01 13:54:31 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,110 @@
+import { useState, useEffect } from "react";
+import "./index.css";
+
+const KEY = "256eb208302740a29cd43347253004";
+function App() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] =useState(null);
+  const[loading, setLoading] = useState(false);
+  const [coords, setCoordds] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+
+
+  useEffect(() => {
+    if(!navigator.geolocation) {
+      setError("Geolocation is not supported by your browser");
+      return;
+    }
+    navigator.geolocation.getCurrentPosition((position) =>{
+      console.log(position);
+      const {latitude, longitude} = position.coords;
+      setCoordds({latitude, longitude});
+    }, (err) => {
+      console.error("Geolocation error", err.message);})
+    setError("Failed to get your location");
+  }, []);
+
+  useEffect(() => {
+    if(!city.trim() && !coords) {
+      setWeatherData(null);
+      setError(null)
+      return;
+    }
+    async function getData() {
+      setLoading(true)
+      try{
+
+        const query = city.trim() ? city : `${coords.latitude},${coords.longitude}`;
+        const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${query}`);
+        const data = await res.json();
+
+        if(data.error) {
+          setError(data.error.message);
+          setWeatherData(null);
+          return;
+        }
+        setWeatherData(data);
+        setError(null);
+      } catch(err) {
+        setError("Failed to fetch weather data");
+        setWeatherData(null);
+      } finally {
+        setLoading(false);
+      }
+
+    }
+    getData();
+  }, [city, coords]);
+
+  function renderError() {
+    return <p>{error}</p>;
+  }
+  function renderLoading() {
+    return <p>Loading...</p>;
+  }
+  function renderWeather() {
+    return (
+        <div className="weather-card">
+          <h2>{`${weatherData?.location?.name}, ${weatherData?.location?.country}`}</h2>
+          <img src={`https:${weatherData?.current?.condition?.icon}`} alt="icon" className="weather-icon"/>
+          <p className="temperature">{Math.round(weatherData?.current?.temp_f)}</p>
+          <p className="condition">{weatherData?.current?.condition?.text}</p>
+          <div className="weather-details">
+            <p>Humidity: {weatherData?.current?.humidity}%</p>
+            <p>Wind: {weatherData?.current?.wind_mph} mp/h</p>
+          </div>
+        </div>
+    );
+  }
+
+  return (
+      <div className="app">
+        <div className="widget-container">
+          <div className="weather-card-container">
+            <h1 className="app-title">Weather Widget</h1>
+            <div className="search-container">
+              <input
+                  value={inputValue}
+                  type="text"
+                  placeholder="Enter city name"
+                  className="search-input"
+                  onChange={(e) => setInputValue(e.target.value)}
+                  onKeyDown={(e) =>{
+                    if(e.key === "Enter"){
+                      setCity(inputValue);
+                    }
+                  }}
+              />
+            </div>
+          </div>
+          {error && renderError()}
+          {loading && renderLoading()}
+          {!loading && !error && weatherData && renderWeather()}
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
